1c34d4d70b02dd49958a0687471f9f30b0c6eaac
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-15 15:48:32 -0700

    IG POST'

--- array.js
+++ array.js
@@ -1,44 +1,88 @@
-var name = 'Rome';
-console.log(name);
-var myLocation = 'Los Angeles';
-console.log(myLocation);
-var favoriteNumber = 5;
-console.log(favoriteNumber);
-var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets'];
-console.log(teams);
-// while loops
-var number = 10;
-var otherNumber = 1;
-while (otherNumber < number) {
-    console.log(otherNumber);
-    // otherNumber += 1;
-    otherNumber = otherNumber + 1; //
-}
-var numberTwo = 20;
-var myOtherNumber = 10;
-while (myOtherNumber < numberTwo) {
-    console.log(myOtherNumber); // 10, 11, 12
-    myOtherNumber += 1; // 11, 12, 13
-}
-var romeFavorite = 5;
-var letGoToOneHundred = 100;
-while (romeFavorite < letGoToOneHundred) {
-    console.log(romeFavorite); // 5, 6, 7
-    romeFavorite += 1; // 6, 7, 8
-}
-// for loops
-for (var i = 0; i < 10; i++) {
-    console.log(i); // 0, 1, 2...
-}
-// print teams
-for (var i = 0; i < teams.length; i++) {
-    var eachTeam = teams[i]; // Rockets, Lakers, Warriors ... [intermediate variable]
-    console.log(eachTeam);
-}
-// Make an array 
-// Use a for loop to iterate through your array
-// print each element in your array
-Collapse
+// // var name = 'Rome';
+// console.log(name);
+// var myLocation = 'Los Angeles';
+// console.log(myLocation);
+// var favoriteNumber = 5;
+// console.log(favoriteNumber);
+// var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets'];
+// console.log(teams);
+// // while loops
+// var number = 10;
+// var otherNumber = 1;
+// while (otherNumber < number) {
+//     console.log(otherNumber);
+//     // otherNumber += 1;
+//     otherNumber = otherNumber + 1; //
+// }
+// var numberTwo = 20;
+// var myOtherNumber = 10;
+// while (myOtherNumber < numberTwo) {
+//     console.log(myOtherNumber); // 10, 11, 12
+//     myOtherNumber += 1; // 11, 12, 13
+// }
+// var romeFavorite = 5;
+// var letGoToOneHundred = 100;
+// while (romeFavorite < letGoToOneHundred) {
+//     console.log(romeFavorite); // 5, 6, 7
+//     romeFavorite += 1; // 6, 7, 8
+// }
+// // for loops
+// for (var i = 0; i < 10; i++) {
+//     console.log(i); // 0, 1, 2...
+// }
+// // print teams
+// for (var i = 0; i < teams.length; i++) {
+//     var eachTeam = teams[i]; // Rockets, Lakers, Warriors ... [intermediate variable]
+//     console.log(eachTeam);
+// }
+// // Make an array 
+// // Use a for loop to iterate through your array
+// // print each element in your array
+// Collapse
+
+// //objects
+// //key value pairs
+
+// const nick = {
+//     fullName: ' Nick Phillips'
+//     age: 36,
+//     myLocation: 'San Francisco'
+//     placesTraveled: [ 'New York', 'Sonoma', 'Los Angeles', 'Sacramento' ]
+//     console.log(nick);
+
+//     //my location
+//     console.log(nick.myLocation);
+
+//     // print Los Angeles
+//     console.log(nick.placesTraveled [2]);
+
+//     const myReallyCoolPost = {
+//         username: '@gerneralassembly',
+//         likes: 1200,
+//         comments: ['cool post', "nice", 'js is cool', 'whoah']
+//         caption: 'Day 3 of SEI',
+//         picture: "https://instagram.com/generalassembly"
+//         shares: 12
+
+//         console.log(myReallyCoolPost);
+//         myReallyCoolPost.location = 'SF';
+//         // console.log(myReallyCoolPost);
+
+    }
+// IG post
+    const nickInstagramPage= {
+        username: '@nickphillipsmusic',
+        likes: 300,
+        comments ['great music', 'beautiful', 'more please'],
+        caption: 'todays jam',
+        picture; "https://instagram.com/nickphillips",
+ 
+        console.log(myReallyCoolPost);
+        nickInstagramPage.location = 'SF';
+        console.log(nickInstagramPage);
+        
+
+    }
 
 
 

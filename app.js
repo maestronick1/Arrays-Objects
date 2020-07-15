63bc5b1a53bc8e723c25653a73ed733c4dc265c9
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-15 14:27:14 -0700

    new push

--- app.js
+++ app.js
@@ -7,4 +7,26 @@ var favoriteNumber = 7;
 console.log(favoriteNumber);
 
 var teams = ['Rockets','Lakers', 'Warriors', 'Clippers', 'Blazers' 'Celtics', 'Nets'];
-console.log(teams)
+console.log(teams);
+
+// while loops
+
+while number = 10;
+var otherNumber = 1;
+While (otherNumber < number) {
+      console.log(otherNumber);
+      // otherNumber +=
+      otherNumber = otherNumber = 1; //
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber); // 10, 11, 12
+    myOtherNumber += 1; // 11, 12, 13       
+
+}
+
+
+
+}

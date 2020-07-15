ca5024e17e3a1a144cdcdc23f153dc1fe76fe65b
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-15 14:04:42 -0700

    teams' ' ' '

---
+++ app.js
@@ -0,0 +1,10 @@
+var name = 'Nick'
+console.log(name);
+
+var location = 'Los Angeles';
+
+var favoriteNumber = 7;
+console.log(favoriteNumber);
+
+var teams = ['Rockets','Lakers', 'Warriors', 'Clippers', 'Blazers' 'Celtics', 'Nets'];
+console.log(teams)
